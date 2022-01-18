ff2441620c44da73423de1b0fdd2adaddb64612f
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-01-18 08:20:18 -0800

    commit

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 class User:
+
   def __init__(self, name):
     self.name = name
     self.amount = 0
@@ -11,3 +12,13 @@ class User:
 
   def display_user_balance(self):
     print(f"User: {self.name}, Balance {self.amount}")
+
+rogan = User("Rogan")  
+josh = User("Josh")
+sara = User("Sara")
+
+
+rogan.make_deposit(10)
+rogan.make_deposit(20)
+rogan.make_deposit(30)
+rogan.make_withdrawlal(10)
