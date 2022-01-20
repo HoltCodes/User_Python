492a11b774374e5e363bf823f4f0cbf37dea4ce4
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-01-19 17:13:39 -0800

    commit

--- user_changing_methods.py
+++ user_changing_methods.py
@@ -6,43 +6,36 @@ class User:
 
   def make_deposit(self, amount):
     self.amount += amount
+    return self
     
 
   def make_withdrawlal(self, amount):
     self.amount -= amount
+    return self
     
 
   def display_user_balance(self):
     print(f"User: {self.name}, Balance {self.amount}")
+    return self
+    
 
   def transfer_money(self, amount, user):
     self.amount -= amount
     user.amount += amount
     self.display_user_balance()  
     user.display_user_balance()
+    return self
 
 rogan = User("Rogan")  
 josh = User("Josh")
 sara = User("Sara")
 
 
-rogan.make_deposit(10)
-rogan.make_deposit(20)
-rogan.make_deposit(30)
-rogan.make_withdrawlal(10)
-rogan.display_user_balance()
+rogan.make_deposit(10).make_deposit(20).make_deposit(30).make_withdrawlal(10).display_user_balance()
 
-josh.make_deposit(20)
-josh.make_deposit(30)
-josh.make_withdrawlal(10)
-josh.make_withdrawlal(20)
-josh.display_user_balance()
+josh.make_deposit(20).make_deposit(30).make_withdrawlal(10).make_withdrawlal(20).display_user_balance()
 
-sara.make_deposit(100)
-sara.make_withdrawlal(20)
-sara.make_withdrawlal(10)
-sara.make_withdrawlal(5)
-sara.display_user_balance()
+sara.make_deposit(100).make_withdrawlal(20).make_withdrawlal(10).make_withdrawlal(5).display_user_balance()
 
 
 rogan.transfer_money(100, sara)
